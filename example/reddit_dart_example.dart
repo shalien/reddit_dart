c617f21e95fdd7feac4783f34941b9fb3c38e36c
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-07-09 09:28:25 +0200

    Final changes for this version

--- example/reddit_dart_example.dart
+++ example/reddit_dart_example.dart
@@ -4,4 +4,9 @@ Future<void> main() async {
   final RedditClient reddit = RedditClient();
 
   Listing sub = await reddit.getSub('beau_gosse');
+
+  var gallery = sub.children.where((element) =>
+      element is Link && element.isGallery != null && element.isGallery!);
+
+  print(gallery.length);
 }

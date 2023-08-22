58b989d01bdf35a88dc20bd6988a67afd344569a
unknown <ophelien.duparc@projetretro.io>
2023-08-22 22:54:42 +0200

    Bump

--- lib/src/data/subreddit.dart
+++ lib/src/data/subreddit.dart
@@ -11,7 +11,7 @@ class Subreddit extends Thing {
   final String? headerImg;
   final List<int>? headerSize;
   final String? headerTitle;
-  final bool over18;
+  final bool? over18;
   final String publicDescription;
   final bool publicTraffic;
   final num subscribers;
